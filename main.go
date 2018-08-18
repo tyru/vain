80b40aef650e419553aa59ce21de7b150f9597e3
tyru <tyru.exe@gmail.com>
2018-08-19 05:56:10 +0900

    feat: add 'vain fmt' command

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 	"sync"
 
 	"github.com/hashicorp/go-multierror"
-
-	"github.com/tyru/vain/node"
 )
 
 func main() {
@@ -23,7 +21,9 @@ func main() {
 	var err error
 	switch os.Args[1] {
 	case "build":
-		err = build(os.Args[2:])
+		err = cmdBuild(os.Args[2:])
+	case "fmt":
+		err = cmdFormat(os.Args[2:])
 	default:
 		usage()
 	}
@@ -43,7 +43,7 @@ COMMAND
 `)
 }
 
-func build(args []string) error {
+func cmdBuild(args []string) error {
 	buildErrs := make(chan error, 16)
 	errs := make([]error, 0, 16)
 	done := make(chan bool, 1)
@@ -65,7 +65,7 @@ func build(args []string) error {
 		for file := range files {
 			wg.Add(1)
 			go func(file string) {
-				if err := genFiles(file); err != nil {
+				if err := buildFile(file); err != nil {
 					buildErrs <- err
 				}
 				wg.Done()
@@ -116,7 +116,7 @@ func collectTargetFiles(args []string, files chan<- string) error {
 	})
 }
 
-func genFiles(name string) error {
+func buildFile(name string) error {
 	src, err := os.Open(name)
 	if err != nil {
 		return err
@@ -129,52 +129,24 @@ func genFiles(name string) error {
 		return err
 	}
 
-	formatCh := make(chan node.Node, 1)
-	analyzeCh := make(chan node.Node, 1)
-
 	lexer := lex(name, content.String())
 	parser := parse(name, lexer.Tokens())
-	formatter := format(name, formatCh)
-	analyzer := analyze(name, analyzeCh)
+	analyzer := analyze(name, parser.Nodes())
 	translator := translate(name, analyzer.Nodes())
 
-	prettifiedFile := name[:len(name)-len(".vain")] + ".vain.pretty"
 	vimFile := name[:len(name)-len(".vain")] + ".vim"
-	writeErr := make(chan error, 2)
-	var wg sync.WaitGroup
+	writeErr := make(chan error, 1)
 
-	// 7. []io.Reader -> Write to file.vim
-	wg.Add(1)
+	// 5. []io.Reader -> Write to file.vim
 	go func() {
 		writeErr <- writeReaders(translator.Readers(), vimFile)
-		wg.Done()
 	}()
 
-	// 6. []jsonExpr -> Translate to vim script -> []io.Reader
+	// 4. []jsonExpr -> Translate to vim script -> []io.Reader
 	go translator.Run()
 
-	// 5. []node.Node -> Check semantic errors, emit intermediate code -> []jsonExpr
+	// 3. []node.Node -> Check semantic errors, emit intermediate code -> []jsonExpr
 	go analyzer.Run()
-
-	// 4.1. []io.Reader -> Write to file.vast
-	wg.Add(1)
-	go func() {
-		writeErr <- writeReaders(formatter.Readers(), prettifiedFile)
-		wg.Done()
-	}()
-
-	// 4. []node.Node -> Format codes -> []io.Reader
-	go formatter.Run()
-
-	// 3. []node.Node -> 4. formatter, 5. analyzer
-	go func() {
-		for node := range parser.Nodes() {
-			formatCh <- node
-			analyzeCh <- node
-		}
-		close(formatCh)
-		close(analyzeCh)
-	}()
 
 	// 2. []token -> Parse -> []node.Node
 	go parser.Run()
@@ -182,15 +154,7 @@ func genFiles(name string) error {
 	// 1. source code -> Lex -> []token
 	go lexer.Run()
 
-	wg.Wait()
-	close(writeErr)
-	errs := make([]error, 0, 2)
-	for err := range writeErr {
-		if err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return multierror.Append(nil, errs...).ErrorOrNil()
+	return <-writeErr
 }
 
 // Write given readers to temporary file with a buffer.
@@ -219,4 +183,89 @@ func writeReaders(readers <-chan io.Reader, dst string) error {
 	}
 	tmpfile.Close()
 	return os.Rename(tmpfile.Name(), dst)
+}
+
+func cmdFormat(args []string) error {
+	buildErrs := make(chan error, 16)
+	errs := make([]error, 0, 16)
+	done := make(chan bool, 1)
+
+	// 3. Collect errors
+	go func() {
+		for err := range buildErrs {
+			errs = append(errs, err)
+		}
+		done <- true
+	}()
+
+	var wg sync.WaitGroup
+	files := make(chan string, 32)
+
+	// 2. files -> Transpile given files -> file.vast, file.vim
+	wg.Add(1)
+	go func() {
+		for file := range files {
+			wg.Add(1)
+			go func(file string) {
+				if err := formatFile(file); err != nil {
+					buildErrs <- err
+				}
+				wg.Done()
+			}(file)
+		}
+		wg.Done()
+	}()
+
+	// 1. Collect .vain files -> files
+	wg.Add(1)
+	go func() {
+		if err := collectTargetFiles(args, files); err != nil {
+			buildErrs <- err
+		}
+		close(files)
+		wg.Done()
+	}()
+
+	wg.Wait()
+	close(buildErrs)
+	<-done
+
+	return multierror.Append(nil, errs...).ErrorOrNil()
+}
+
+func formatFile(name string) error {
+	src, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	var content strings.Builder
+	_, err = io.Copy(&content, src)
+	src.Close()
+	if err != nil {
+		return err
+	}
+
+	lexer := lex(name, content.String())
+	parser := parse(name, lexer.Tokens())
+	formatter := format(name, parser.Nodes())
+
+	vimFile := name + ".pretty"
+	done := make(chan error, 1)
+
+	// 4. []io.Reader -> Write to file.vim
+	go func() {
+		done <- writeReaders(formatter.Readers(), vimFile)
+	}()
+
+	// 3. []node.Node -> Format codes -> []io.Reader
+	go formatter.Run()
+
+	// 2. []token -> Parse -> []node.Node
+	go parser.Run()
+
+	// 1. source code -> Lex -> []token
+	go lexer.Run()
+
+	return <-done
 }
